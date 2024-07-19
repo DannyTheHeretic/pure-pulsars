f457f8aad02f54156aeca1a314b4c79f31787cc0
Koisu <koisuspotify@gmail.com>
2024-07-19 13:52:25 -0600

    added user class

--- src/database/user.py
+++ src/database/user.py
@@ -1,20 +1,21 @@
-from collections.abc import Iterable
-
-
-class User:
-    def __init__(self, name, leaderboard_position, score, times_played, wins, failure) -> None:
+class User: #noqa: D101
+    def __init__( #noqa: PLR0913
+        self, name: str, leaderboard_position: int, score: int, times_played: int, wins: int, failure: int,
+    ) -> None:
         self.name = name
         self.leaderboard_position = leaderboard_position
         self.score = score
-        self.times_played = times_played 
+        self.times_played = times_played
         self.wins = wins
         self.failure = failure
-    def to_dictionary(self):
+
+    def to_dictionary(self) -> dict:
+        """Convert it to a dictionary."""
         return {
-            'name' : self.name,
-            'leaderboard_position' : self.leaderboard_position,
-            'score' : self.score,
-            'times_played' : self.times_played,
-            'wins' : self.wins,
-            'failure' : self.failure
+            "name": self.name,
+            "leaderboard_position": self.leaderboard_position,
+            "score": self.score,
+            "times_played": self.times_played,
+            "wins": self.wins,
+            "failure": self.failure,
         }
