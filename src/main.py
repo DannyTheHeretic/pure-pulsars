484e3df6440f52efdb09947060622fcf094c32bb
Koisu <koisuspotify@gmail.com>
2024-07-19 15:37:51 -0600

    Added print ready

--- src/main.py
+++ src/main.py
@@ -2,9 +2,11 @@ import os
 
 import discord
 from discord import app_commands
+from dotenv import load_dotenv
 
 from cmds import wikiguesser, wikirandom
 
+load_dotenv(".env")
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
@@ -12,6 +14,7 @@ tree = app_commands.CommandTree(client)
 
 @client.event
 async def on_ready() -> None:  # noqa: D103
+    print("ready")
     await tree.sync(guild=discord.Object(id=1262497899925995563))
 
 
