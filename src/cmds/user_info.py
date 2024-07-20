93a57bd549b97890ac4a7b091318cef749b201c8
Danny <DannyTheHeretic@gmail.com>
2024-07-20 16:40:35 -0500

    fix(pure-pulsers): :bug: Database fixing.
    Added some singleton attr. to the database class.

--- src/cmds/user_info.py
+++ src/cmds/user_info.py
@@ -1,34 +1,32 @@
 import discord
 from discord import app_commands
 
-from database.database_core import Database, NullUserError
+from database.database_core import NullUserError
+from main import DATA
 
-def main(tree: app_commands.CommandTree, data) -> None:
-    """Create user-info command"""
+
+def main(tree: app_commands.CommandTree) -> None:
+    """Create user-info command."""
 
     @tree.command(
         name="user-info",
         description="Returns your stats",
-        guild=discord.Object(id=1262497899925995563)
+        guild=discord.Object(id=1262497899925995563),
     )
     async def user_info(interaction: discord.Interaction, user: discord.User) -> None:
         await interaction.response.defer(thinking=True)
         embed = discord.embeds.Embed()
         try:
-            user_data = data.get_user(user.id)
+            user_data = DATA.get_user(interaction.guild_id, user.id)
             for key, value in user_data.items():
                 if key.lower() == "userid":
                     continue
                 embed.add_field(
-                    name=key.capitalize()
-                    .replace("_", " ")
-                    .replace("Failure", "Failures"),
+                    name=key.capitalize().replace("_", " ").replace("Failure", "Failures"),
                     value=value,
                     inline=False,
                 )
         except NullUserError:
-            await interaction.followup.send(
-                content=f"User: {user.mention} has **not** been added"
-            )
+            await interaction.followup.send(content=f"User: {user.mention} has **not** been added")
 
         await interaction.followup.send(embed=embed)
