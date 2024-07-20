c238b602a4329ecf9a2482f257b3f8abf33e43f7
Danny <DannyTheHeretic@gmail.com>
2024-07-20 18:16:08 -0500

    leaderboard

--- src/cmds/leaderboard.py
+++ src/cmds/leaderboard.py
@@ -22,8 +22,8 @@ def main(tree: app_commands.CommandTree) -> None:
     async def leaderboard(interaction: discord.Interaction, glob: _GloSer = _GloSer.SERVER) -> None:
         ser_id = interaction.guild_id if bool(glob.value) else 0
         await interaction.response.defer(thinking=True)
-        board = await DATA.get_server(ser_id).values()
-        lead = list(board)
+        board = await DATA.get_server(ser_id)
+        lead = list(board.values())
         lead.sort(key=_sort_leaders, reverse=True)
         lead = lead[0:10]
         embed = discord.Embed(
