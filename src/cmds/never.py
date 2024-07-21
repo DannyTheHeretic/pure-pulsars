1b0fb413dcd49d23bcc4f7a294fa422847e411e4
koisu-unavailble <koisuspotify@gmail.com>
2024-07-21 15:23:26 -0700

    chore: :recycle: ruff check
    ruff check

--- src/cmds/never.py
+++ src/cmds/never.py
@@ -1,9 +1,11 @@
+
+import asyncio
+
 import discord
 from discord import app_commands
-import time
 
-from database.database_core import DATA
-from wikiutils import search_wikipedia, make_embed
+from wikiutils import make_embed, search_wikipedia
+
 
 def main(tree: app_commands.CommandTree) -> None:
     """Create a wikiguesser command."""
@@ -16,7 +18,7 @@ def main(tree: app_commands.CommandTree) -> None:
     async def never_gonna_give_you_up(interaction: discord.Interaction, user: discord.User) -> None:
         await interaction.response.send_message(f"Starting deathmatch between {user.mention} and {interaction.user.mention}...")
         article = await search_wikipedia("Never gonna give you up")
-        time.sleep(1)
+        asyncio.sleep(1)
         await interaction.followup.send(embed=make_embed(article))
-       
+
 
