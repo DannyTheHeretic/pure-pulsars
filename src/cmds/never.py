8c3f3b5f33e4c904456c2e2d713a54ab65ed52c0
Daniel <90278923+DannyTheHeretic@users.noreply.github.com>
2024-07-23 19:53:13 -0500

    feat: Added a more comprehensive rick roll.

--- src/cmds/never.py
+++ src/cmds/never.py
@@ -17,8 +17,11 @@ def main(tree: app_commands.CommandTree) -> None:
     )
     async def never_gonna_give_you_up(interaction: discord.Interaction, user: discord.User) -> None:
         await interaction.response.send_message(f"Starting deathmatch between {user.mention} and {interaction.user.mention}...")
-        article = await search_wikipedia("Never gonna give you up")
+        article = await search_wikipedia("Rickrolling")
         await asyncio.sleep(1)
-        await interaction.followup.send(embed=make_embed(article))
+        embed = make_embed(article)
+        embed.description = f"{article.extract(chars=400)}...([read more](https://www.youtube.com/watch?v=dQw4w9WgXcQ))"
+        embed.set_image(url="https://upload.wikimedia.org/wikipedia/en/f/f7/RickRoll.png")
+        await interaction.followup.send(embed=embed)
 
 
