0ab52a5cd1024d98053cf729498e9c8ab929073c
teald <teal.dillon@gmail.com>
2024-07-27 21:38:20 -0700

    docs(bot): ✍️ Slightly modify and add some docstrings in database core source.

--- src/database/database_core.py
+++ src/database/database_core.py
@@ -1,3 +1,5 @@
+"""Core funcitonaliry for interactions with the scoring database."""
+
 import os
 from typing import ClassVar, Literal
 
@@ -43,7 +45,15 @@ class DatabaseController:
         user: UserController,
         guild_id: int,
     ) -> None:
-        """Add a user to the database using ref.set."""
+        """Add a user to the database using ref.set.
+
+        Args:
+        ----
+        user_id (int): The user's id.
+        user (UserController): The user object.
+        guild_id (int): The guild id.
+
+        """
         database_user = await self.conn(guild_id, user_id)
         _new_user = user.to_dictionary()
         database_user.set(_new_user)
@@ -55,7 +65,20 @@ class DatabaseController:
         value: int,
         key: Literal["last_played", "score", "times_played", "wins", "failure"],
     ) -> None:
-        """Update the specified value for the specified user."""
+        """Update the specified value for the specified user.
+
+        Args:
+        ----
+        guild_id (int): The guild id.
+        user_id (str): The user id.
+        value (int): The updated score.
+        key (Literal["last_played", "score", "times_played", "wins", "failure"]): The key to update.
+
+        Raises:
+        ------
+        NullUserError: If the user doesn't exist.
+
+        """
         """self._ref = db.reference("/users")"""
         database_user = await self.conn(guild_id, user_id)
         if database_user.get():
@@ -64,25 +87,63 @@ class DatabaseController:
         raise NullUserError
 
     async def get_user(self, guild_id: int, user_id: int) -> dict[str, int | str]:
-        """Return a user a dict."""
+        """Return a user's information as a dict.
+
+        Args:
+        ----
+        guild_id (int): The guild id.
+        user_id (int): The user id.
+
+        Returns:
+        -------
+        dict[str, int | str]: The user's information.
+
+        Raises:
+        ------
+        NullUserError: If the user doesn't exist.
+
+        """
         database_user = await self.conn(guild_id, user_id)
         if database_user.get():
             return database_user.get()
         raise NullUserError
 
     async def get_server(self, guild_id: int) -> dict:
+        """Get server information as a dict.
+
+        Args:
+        ----
+        guild_id (int): The guild id.
+
+        Returns:
+        -------
+        dict: The server information.
+
+        """
         """Get the specified server."""
         _ref = db.reference(f"/server/{guild_id}/")
         return _ref.get()
 
     async def get_all_servers(self) -> list:
-        """Return all servers."""
+        """Return all servers' information as a list."""
         return db.reference("/server/").get()
 
     async def conn(self, guild_id: int, user_id: int) -> db.Reference:
-        """Return a connection to the db."""
+        """Return a connection to the db, for specific guilds/users.
+
+        Args:
+        ----
+        guild_id (int): The guild id.
+        user_id (int): The user id.
+
+        Returns:
+        -------
+        db.Reference: The reference to the user.
+
+        """
         _ref = db.reference(f"/server/{guild_id}/")
         return _ref.child(str(user_id))
 
 
+# Pre-instantiated controller object.
 DATA = DatabaseController()
