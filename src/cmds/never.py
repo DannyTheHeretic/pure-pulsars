7f5d082ed16c7fad572d103e6dbef7bd8e0e519d
teald <teal.dillon@gmail.com>
2024-07-27 20:58:22 -0700

    docs(bot): ✍️ Add docstrings in never command source.

--- src/cmds/never.py
+++ src/cmds/never.py
@@ -1,3 +1,5 @@
+"""'Never' command absolutely not meant to be used ever."""
+
 import asyncio
 
 import discord
@@ -7,13 +9,21 @@ from wikiutils import make_embed, search_wikipedia
 
 
 def main(tree: app_commands.CommandTree) -> None:
-    """Create a wikiguesser command."""
+    """Create a never command."""
 
     @tree.command(
         name="never",
         description="Who knows what this does.",
     )
     async def never_gonna_give_you_up(interaction: discord.Interaction, user: discord.User) -> None:
+        """Run the never command.
+
+        Args:
+        ----
+        interaction (discord.Interaction): The interaction object.
+        user (discord.User): The user to challenge.
+
+        """
         await interaction.response.send_message(
             f"Starting deathmatch between {user.mention} and {interaction.user.mention}..."
         )
